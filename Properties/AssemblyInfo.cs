6885da345438d334e02eabca4fa88e269a1170b0
vandenbrande <contact@arduinosensors.nl>
2017-07-11 22:13:14 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlueCore Tech Servo Control")]
+[assembly: AssemblyDescription("Open Source Servo Control for robotarm with automatic program and manual control panel. If you found this program somewhere on the internet and cannot find the arduino code feel free to send a mail to contact@arduinosensors.nl, Danny van den Brande")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BlueCore Tech")]
+[assembly: AssemblyProduct("BlueCore Tech Servo Control")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b5162a6-06e0-46ec-b309-06383c1bec63")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
